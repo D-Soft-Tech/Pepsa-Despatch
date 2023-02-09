759020199663d9ae1036339ee968c8a8d69218cd
DSoftTech <81322397+DSoftTech@users.noreply.github.com>
2023-02-09 04:12:06 +0100

    Added dependencies for project setup

---
+++ app/src/main/java/com/pepsa/pepsadispatch/shared/utils/di/NetworkModule.kt
@@ -0,0 +1,10 @@
+package com.pepsa.pepsadispatch.shared.utils.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+}
